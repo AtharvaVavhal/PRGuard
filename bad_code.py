46b1c5204962aea2c176ccfc8a3341034a150e3c
AtharvaVavhal <atharvavavhal@gmail.com>
2026-03-20 22:12:12 +0530

    Add user processing logic

---
+++ bad_code.py
@@ -0,0 +1,14 @@
+import requests
+
+def process(d, db):
+    if d is None:
+        return None
+    if "id" not in d:
+        return None
+    res = db.query("SELECT * FROM users WHERE id=" + str(d["id"]))
+    if res:
+        tmp = res[0]
+        payload = {"uid": tmp["id"], "n": tmp["name"]}
+        requests.post("https://api.example.com/notify", json=payload)
+        return payload
+    return None
